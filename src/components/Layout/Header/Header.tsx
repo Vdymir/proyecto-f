55060e02d4bd7e26c080da6618560ed4edd757d2
Vladimir <vladistar200302@gmail.com>
2021-12-14 14:17:49 -0500

    header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,10 +1,25 @@
+import { Link } from 'react-router-dom';
+import { URL_GITHUB } from '../../../constants';
 import Navbar from '../Navbar';
 import './index.css';
 
 const Header = () => {
+
+    const handleClick = () => window.open(URL_GITHUB)
+    
     return (
-        <header className='header'> 
+        <header className='header'>
             <Navbar />
+            <div className='header__container'>
+                <div className='header__container-logos'>
+                    <div className='header__logos' />
+                </div>
+                <div className="header__container-text">
+                    <h1>¿PROYECTO <span>F</span>?</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, <span>adipisicing</span> elit. Non expedita quisquam ad aut deserunt repellendus <span>suscipit</span> amet fugiat incidunt harum a similique, ipsa odio dicta? Iusto <span>suscipit</span> dolor quia at.</p>
+                    <p>By: <span onClick={handleClick} >Vdymir.</span></p>
+                </div>
+            </div>
         </header>
     )
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const URL_GITHUB = 'https://github.com/Vdymir';
